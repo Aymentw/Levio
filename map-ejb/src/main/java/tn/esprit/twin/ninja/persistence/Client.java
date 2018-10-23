a27cb424148c5452d28c184ffdcca8a1613b5d43
anonymmed <mohamed.abdelhafidh@esprit.tn>
2018-10-23 19:48:50 +0100

    adding Client Entity

---
+++ map-ejb/src/main/java/tn/esprit/twin/ninja/persistence/Client.java
@@ -0,0 +1,51 @@
+package tn.esprit.twin.ninja.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+
+enum clientCategory {
+	privateCat, publicCat;
+}
+enum clientType {
+	currentClient, newClient, finishedContract;
+}
+@Entity
+public class Client {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private clientCategory category;
+	private clientType type;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public clientCategory getCategory() {
+		return category;
+	}
+	public void setCategory(clientCategory category) {
+		this.category = category;
+	}
+	public clientType getType() {
+		return type;
+	}
+	public void setType(clientType type) {
+		this.type = type;
+	}
+	
+	
+}
+	
