8c7a6ff6bd8c6b203c8e21a69c73d5e30961be93
Firassov <firas.mrad@esprit.tn>
2018-10-30 22:47:53 +0100

    Dashboardservice

--- map-ejb/src/main/java/tn/esprit/twin/ninja/services/DashboardService.java
+++ map-ejb/src/main/java/tn/esprit/twin/ninja/services/DashboardService.java
@@ -150,35 +150,74 @@ public class DashboardService implements DashboardServicesLocal {
 	@Override
 	public void reportResource(int ressourceId) throws IOException {//okish
 		Ressource r=em.find(Ressource.class, ressourceId);
-		String html="<html><head><style></style></head><body>";
-		html+="<p>First name : "+r.getFirst_name()+"</p><br>";
-		html+="<p>Last name : "+r.getLast_name()+"</p><br>";
-		html+="<p>Contract Type : "+r.getContract_type()+"</p><br>";
-		html+="<p>Leaves List :</p><br>";
+		String html="<html><head><style type=\"text/css\">\r\n" + 
+				"        * { margin: 0; padding: 0; }\r\n" + 
+				"        body { font: 16px Helvetica, Sans-Serif; line-height: 24px; background: url(images/noise.jpg); }\r\n" + 
+				"        .clear { clear: both; }\r\n" + 
+				"        #page-wrap { width: 800px; margin: 40px auto 60px; }\r\n" + 
+				"        #pic { float: right; margin: -30px 0 0 0; }\r\n" + 
+				"        h1 { margin: 0 0 16px 0; padding: 0 0 16px 0; font-size: 42px; font-weight: bold; letter-spacing: -2px; }\r\n" + 
+				"        h2 { font-size: 20px; margin: 0 0 6px 0; position: relative; }\r\n" + 
+				"        h2 span { position: absolute; bottom: 0; right: 0; font-style: italic; font-family: Georgia, Serif; font-size: 16px; color: #999; font-weight: normal; }\r\n" + 
+				"        p { margin: 0 0 16px 0; }\r\n" + 
+				"        a { color: #999; text-decoration: none; border-bottom: 1px dotted #999; }\r\n" + 
+				"        a:hover { border-bottom-style: solid; color: black; }\r\n" + 
+				"        ul { margin: 0 0 32px 17px; }\r\n" + 
+				"        #objective { width: 500px; float: left; }\r\n" + 
+				"        #objective p { font-family: Georgia, Serif; font-style: italic; color: #666; }\r\n" + 
+				"        dt { font-style: italic; font-weight: bold; font-size: 18px; text-align: right; padding: 0 26px 0 0; width: 150px; float: left; height: 100px; border-right: 1px solid #999;  }\r\n" + 
+				"        dd { width: 600px; float: right; }\r\n" + 
+				"        dd.clear { float: none; margin: 0; height: 15px; }\r\n" + 
+				"     </style></head><body><div id=\"page-wrap\">\r\n" + 
+				"    \r\n" + 
+				"        <img src=\"images/Levio.png\" width=\"300\" height=\"120\" alt=\"Levio\" id=\"pic\" />\r\n" + 
+				"    \r\n" + 
+				"        <div id=\"contact-info\" class=\"vcard\">\r\n" + 
+				"        \r\n" + 
+				"            <!-- Microformats! -->\r\n" + 
+				"        \r\n" + 
+				"            <h1 class=\"fn\">Resource report</h1>\r\n" + 
+				"            <dl>\r\n" + 
+				"            <dd class=\"clear\"></dd>\r\n" + 
+				"            <dt>Basic Information</dt>";
+		html+="<dd><p><strong>First name :</strong> "+r.getFirst_name()+"</p>";
+		html+="<p><strong>Last name :</strong> "+r.getLast_name()+"</p>";
+		html+="<p><strong>Contract Type :</strong> "+r.getContract_type()+"</p></dd>"
+				+ "<div class=\"clear\"></div>"
+				+ "<hr>";
+		html+="<dt>Leaves List :</dt>"
+				+ "<dd><ul>";
 		int i=0;
 		for(Leave l : r.getLeaves()){
 			i++;
-			html+="<p>"+i+"- ";
-			html+="Start Date : "+l.getStart_date();
-			html+="		End Date : "+l.getEnd_date()+"</p><br>";
+			html+="<li>";
+			html+="<strong>Start Date :</strong> "+l.getStart_date();
+			html+="		<strong>End Date :</strong> "+l.getEnd_date()+"</li>";
 		}
-		html+="<p>Mandates affected to resource : </p><br>";
+		html+="</ul></dd>"
+				+ "<dd class=\"clear\"></dd>"
+				+ "<hr>"
+				+ "<dt>Mandates affected to resource : </dt><dd>";
 		for(Mandate m : r.getMandate()){
-			html+="-------------------------------------------------------------------<br>";
-			html+="Mandate : "+m.getId()+"<br>";
-			html+="Start Date : "+m.getStartDate()+"<br>";
+			html+="<h2>Mandate : "+m.getId()+"</h2>";
+			html+="<p>Start Date : "+m.getStartDate()+"<br>";
 			html+="End Date : "+m.getEndDate()+"<br>";
 			html+="Charges : "+m.getMontant()+"<br>";
-			html+="Project "+m.getProject().getName()+" for Client \""+m.getProject().getClients().getName()+"\"<br>";
+			html+="Project <strong>"+m.getProject().getName()+"</strong> for Client <strong>"+m.getProject().getClients().getName()+"</strong><br></p>";
 			html+="-------------------------------------------------------------------<br>";
 		}
-		html+="Skills : <br>";
+		html+="</dd>"
+				+ "<dd class=\"clear\"></dd>"
+				+ "<hr>"
+				+ "<dt>Skills :</dt>"
+				+ "<dd>";
 		for(Skill s : r.getSkills()){
-			html+="-------------------------------------------------------------------<br>";
-			html+=""+s.getName()+"<br>";
-			html+="Rating : "+s.getRating()+"<br>";
-			html+="-------------------------------------------------------------------<br>";
+			html+="<ul>";
+			html+="<li><strong>Skill :</strong>"+s.getName()+"</li>";
+			html+="<li><strong>Rating :</strong> "+s.getRating()+"</li>";
+			html+="</ul>";
 		}
+		html+="</dd></dl><div class=\"clear\"></div></div></body></html>";
 		File file = new File("C:/Users/Firassov/Desktop/reports/report_"+r.getFirst_name()+"_"+r.getLast_name()+".html");
 		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
 		bw.write(html);
