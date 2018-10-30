9417db8a1299e9bcb571c157df024f790c195f24
Achraf <achraf.douss@esprit.tn>
2018-10-31 00:12:20 +0100

    fix

--- map-ejb/src/main/java/tn/esprit/twin/ninja/services/RessourceService.java
+++ map-ejb/src/main/java/tn/esprit/twin/ninja/services/RessourceService.java
@@ -1,25 +1,49 @@
 package tn.esprit.twin.ninja.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import tn.esprit.twin.ninja.interfaces.RessourceServiceLocal;
-import tn.esprit.twin.ninja.persistence.Client;
 import tn.esprit.twin.ninja.persistence.Leave;
-import tn.esprit.twin.ninja.persistence.Message;
 import tn.esprit.twin.ninja.persistence.Project;
 import tn.esprit.twin.ninja.persistence.Ressource;
-import tn.esprit.twin.ninja.persistence.Skill;
+import tn.esprit.twin.ninja.communication.MailSender;
+import tn.esprit.twin.ninja.persistence.*;
+
+import javax.ejb.Stateless;
+import javax.mail.MessagingException;
+import java.util.Set;
 
 @Stateless
 public class RessourceService implements RessourceServiceLocal {
 
-	@PersistenceContext(unitName = "LevioMap-ejb")
+	@PersistenceContext(unitName="LevioMap-ejb")
 	EntityManager em;
 
+	public List<Ressource> getRessourceBySkills(Set<Skill> skills) {
+		TypedQuery<Ressource> query = em.createQuery("SELECT r FROM Ressource r where r.state = 'available'",Ressource.class);
+		query.getResultList().stream().forEach(p->System.out.println("eeeeeee"));
+
+		List<Ressource> availableResources = query.getResultList();
+		List<Ressource> resources = new ArrayList<>();
+		for (Ressource r : availableResources) {
+		for(Skill s : r.getSkills()) {
+			for(Skill rs : skills) {
+				if(s.getName() == s.getName()) {
+					resources.add(r);
+				}
+			}
+		}
+		}
+		return resources;
+		
+		
+	
+		}
 	@Override
 	public void addRessource(Ressource r) {
 
@@ -126,12 +150,67 @@ public class RessourceService implements RessourceServiceLocal {
 	}
 
 	@Override
-	public void sendMessageToClient(Message message, int clientId) {
+	public void sendMessageToClient(Message message,int currentResource, int clientId) throws MessagingException {
+		Ressource ressource = em.find(Ressource.class, currentResource);
 		Client client = em.find(Client.class, clientId);
-		message.setTarget("CLIENT");
-		em.merge(message);
-		// send an email to the client email with the message body
+		Conversation conversation = new Conversation();
+		conversation.setFromUser(ressource);
+		conversation.setToUser(client);
+		conversation.setState("open");
+		em.persist(new Conversation());
+		message.setConversation(conversation);
+		em.persist(message);
+		MailSender mailSender = new MailSender();
+		mailSender.sendMessage(
+				"smtp.gmail.com",
+				"mohamed@pixelwilderness.com",
+				"V4Vendetta",
+				"587",
+				"true",
+				"true",
+				client.getEmail(),
+				message.getSubject()+ ": " + message.getType(),
+				message.getMessage()
+		);
 
+	}
+
+	@Override
+	public List<Conversation> getOpenedConversations(int resourceId) {
+		return em.createQuery("SELECT c FROM Conversation c where c.state = 'open' and (c.fromUser = :user OR c.toUser = :user)", Conversation.class).setParameter("user", resourceId).getResultList();
+	}
+
+	@Override
+	public void respondToAMessage(int conversationId,int currencResource, Message message) throws MessagingException {
+		Conversation conversation = em.find(Conversation.class, conversationId);
+		Ressource cr = em.find(Ressource.class, currencResource);
+		String recipient = (conversation.getToUser().getEmail().equals(cr.getEmail())) ? conversation.getFromUser().getEmail() : conversation.getToUser().getEmail();
+		message.setConversation(conversation);
+		em.persist(message);
+		MailSender mailSender = new MailSender();
+		mailSender.sendMessage(
+				"smtp.gmail.com",
+				"mohamed@pixelwilderness.com",
+				"V4Vendetta",
+				"587",
+				"true",
+				"true",
+				recipient,
+				message.getSubject()+ ": " + message.getType(),
+				message.getMessage()
+		);
+
+	}
+	@Override
+	public List<Conversation> getConversationByType(int currentResource, MessageType messageType) {
+		Ressource cr = em.find(Ressource.class, currentResource);
+		return  em.createQuery("SELECT  m.conversation from Message m where m.conversation.state = 'open' and m.type = :msgType and (m.conversation.toUser.email = :user or m.conversation.fromUser.email = :user) order by m.createDate", Conversation.class).setParameter("msgType" , messageType).setParameter("user", cr.getEmail()).getResultList();
+	}
+
+	@Override
+	public List<Conversation> getConversationBySubject(String subject, int currentResource) {
+		Ressource cr = em.find(Ressource.class, currentResource);
+		return  em.createQuery("select m.conversation from Message m where m.subject LIKE :sub and (m.conversation.fromUser.email = :current or m.conversation.toUser.email = :current)", Conversation.class).setParameter("sub", "%"+subject+"%").setParameter("current", cr.getEmail()).getResultList();
 	}
 
 	@Override
