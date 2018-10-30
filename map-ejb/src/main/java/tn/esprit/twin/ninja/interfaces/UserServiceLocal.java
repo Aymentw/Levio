4b823384e0df577d7f7079141bd0675a55c62587
Achraf <achraf.douss@esprit.tn>
2018-10-30 23:41:43 +0100

    authen

--- map-ejb/src/main/java/tn/esprit/twin/ninja/interfaces/UserServiceLocal.java
+++ map-ejb/src/main/java/tn/esprit/twin/ninja/interfaces/UserServiceLocal.java
@@ -3,6 +3,7 @@ package tn.esprit.twin.ninja.interfaces;
 import javax.ejb.Local;
 
 import tn.esprit.twin.ninja.persistence.Request;
+import tn.esprit.twin.ninja.persistence.User;
 
 @Local
 public interface UserServiceLocal {
@@ -10,5 +11,6 @@ public interface UserServiceLocal {
 	public boolean hasRole(int userId, String role);
 	public void treatRequest(int userId, int requestId);
 	public void untreatRequest(int userId, int requestId);
+	public User Authenticate(String email, String pwd);
 
 }
